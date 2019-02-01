073ab1a4f5e18129d126b1d2ab23dc0ce89556f0
RvClariss <clarissa.rvs345@gmail.com>
2019-02-01 14:15:36 -0700

    finishedgit add .git add .!

--- java.js
+++ java.js
@@ -35,7 +35,7 @@ $("#submit").on("click", function (event) {
 
     // Logs everything to console
     console.log(newTrain.name);
-    console.log(newTrain.desination);
+    console.log(newTrain.destination);
     console.log(newTrain.time);
     console.log(newTrain.frequency);
 
@@ -66,45 +66,49 @@ database.ref().on("child_added", function (childSnapshot) {
     console.log(trainTime);
     console.log(trainFreq);
 
-    var time = calculateTimes(trainFreq, trainTime);
+    var currentTime = moment();
+    console.log("Current Time: " + currentTime);
+    
+    var convertedTrainTime = moment(trainTime, 'HH:mm').subtract(1, 'year');
+    console.log("Converted Time " + convertedTrainTime);
+
+    //var tFrequency = frequency;
+   // console.log("Train Time: " + trainTime);
+
+    //Time is 3:30am
+    //var firstTime = trainTime;
+
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    //var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+
+    // Difference between the times
+    var diffTime = moment().diff(moment(convertedTrainTime), "minutes");
+    console.log("Difference in Time: " + diffTime);
+
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainFreq;
+
+    // Minute Until Train
+    var tMinutesTillTrain = trainFreq - tRemainder;
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
+
 
     // Create the new row
     var newRow = $("<tr>").append(
         $("<td>").text(trainName),
         $("<td>").text(trainDest),
         $("<td>").text(trainFreq),
-        $("<td>").text(time.nextTrain),
-        $("<td>").text(time.minutesTillTrain),
+        $("<td>").text(nextTrain),
+        $("<td>").text(tMinutesTillTrain),
     );
 
     // Append the new row to the table
     $("#table-div > tbody").append(newRow);
 });
 
-function calculateTimes(frequency, startTime) {
-    var tFrequency = frequency;
 
-    //Time is 3:30am
-    var firstTime = startTime;
+    
 
-    // First Time (pushed back 1 year to make sure it comes before current time)
-    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
-
-    // Difference between the times
-    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-
-    // Time apart (remainder)
-    var tRemainder = diffTime % tFrequency;
-
-    // Minute Until Train
-    var tMinutesTillTrain = tFrequency - tRemainder;
-
-    // Next Train
-    var nextTrain = moment().add(tMinutesTillTrain, "minutes").format("hh:mm");
-
-    // return [tMinutesTillTrain, nextTrain];
-    return {
-        minutesTillTrain: tMinutesTillTrain,
-        nextTrain: nextTrain
-    }
-}
+    
